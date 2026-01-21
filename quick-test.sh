1e697d36e652f247772823dbc477bf2f7551a2af
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2026-01-21 14:59:56 -0600

    quick-test.sh updated

--- quick-test.sh
+++ quick-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cargo build --release
-sudo $(pwd)/target/release/cephfs-find-wide-dirs -m 0 / | grep -v '^/proc' | sed 's/ 0$//' | sort > me.test
-sudo fd --unrestricted --type d . / | grep -v '^/proc' | sort > fd.test
+sudo $(pwd)/target/release/cephfs-find-wide-dirs -m 0 /home | grep -v '^/proc' | sed 's/ 0$//' | sort > me.test
+sudo fd --unrestricted --type d . /home | grep -v '^/proc' | sort > fd.test
 
 diff -u me.test fd.test
